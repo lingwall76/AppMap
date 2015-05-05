313b7253e7c1b5636342822ecdff061ecbef0b4e
Kaleo <kauakim@gmail.com>
2015-05-05 16:18:12 -0600

    done

---
+++ AppMap/ThirdViewController.m
@@ -0,0 +1,41 @@
+//
+//  ThirdViewController.m
+//  AppMap
+//
+//  Created by Kaleo Kim on 5/5/15.
+//  Copyright (c) 2015 DevMountain. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *eenyView;
+@property (weak, nonatomic) IBOutlet UIButton *meenyView;
+
+@property (strong, nonatomic) IBOutlet UIView *mineyView;
+
+@end
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
